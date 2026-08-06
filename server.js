9baf204cc0e812e46675bfb9f8cf4e787756038d
davidyassa <davidyassa@hotmail.com>
2026-08-06 17:29:45 +0300

    Stage 3: create with validation

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
-let tasks = [
-  {
-    id: 1,
-    title: "first",
-    done: true,
-  },
-  {
-    id: 67,
-    title: "second",
-    done: false,
-  },
-  {
-    id:69,
-    title: "third",
-    done: false,
-  },
-]
+class Task {
+  id;
+  title;
+  done;
+
+  constructor(title, array, done = false) {
+    this.id = generateId(array);
+    this.title = title;
+    this.done = done;
+  }
+
+  isDone() {
+    return this.done;
+  }
+
+  changeStatus(done = null) {
+    if (done) {
+      this.done = done;
+    }
+    else {
+      this.done = !this.done;
+    }
+  }
+
+  changeTitle(title) {
+    this.title = title;
+  }
+
+}
+
+let tasks = []
+tasks.push(new Task("first", tasks, done = true));
+tasks.push(new Task("second", tasks));
+tasks.push(new Task("third", tasks));
 
 app.get("/", (req, res) => {
   res.json({
@@ -35,8 +53,7 @@ app.get("/tasks", (req, res) => {
   res.json(tasks);
 });
 
-app.get("/tasks/:id", (req,res) => {
-  // res.json(tasks[id])
+app.get("/tasks/:id", (req, res) => {
   const id = Number(req.params.id);
   const task = tasks.find((t) => t.id === id);
 
@@ -47,12 +64,26 @@ app.get("/tasks/:id", (req,res) => {
   res.json(task);
 });
 
+app.post("/tasks", (req, res) => {
+  const title = req.body.title;
+  if (!title) {
+    return res.status(400).json({ error: `Title is empty` });
+  }
+
+  const task = new Task(title, tasks);
+  tasks.push(task);
+  return res.status(201).json(task);
+})
 
 
 
 
-
-
+function generateId(array) {
+  if (!array.length) return 1;
+  // `...` unpacks the list of IDs returned, like python's `*args`
+  const maxId = Math.max(...array.map((t) => t.id));
+  return maxId + 1;
+}
 
 
 
